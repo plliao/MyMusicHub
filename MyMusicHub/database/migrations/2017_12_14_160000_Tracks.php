25c7c494b2951f68d6ee295bce75e4ce246b507c
charson <chl566@nyu.edu>
2017-12-14 15:41:32 -0500

    update Tracks table

--- MyMusicHub/database/migrations/2017_12_14_160000_Tracks.php
+++ MyMusicHub/database/migrations/2017_12_14_160000_Tracks.php
@@ -17,12 +17,10 @@ class Tracks extends Migration
 	Schema::create ( 'Tracks', function (Blueprint $table) { 
 	$table->string ( 'TrackId',22 )->primary();
 	$table->string ( 'TrackName',200 );
+	$table->string ( 'ArtistId',22 );
 	$table->integer( 'TrackDuration');
-	$table->string ( 'TrackArtist', 22 );
-	$table->string ( 'AlbumId' );
 
-	$table->foreign('TrackArtist')->references('ArtistId')->on('Artists');	
-	$table->foreign('AlbumId')->references('AlbumId')->on('Albums');
+	$table->foreign('ArtistId')->references('ArtistId')->on('Artists');
     	});
     }
 
