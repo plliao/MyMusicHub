d41ceba17caacb4bf02bb91d60a55ea3519eebd8
Pei-Lun Liao <pll273@nyu.edu>
2017-12-14 15:13:40 -0500

    convert str to datetime in Album sedder

--- MyMusicHub/database/seeds/AlbumsSeeder.php
+++ MyMusicHub/database/seeds/AlbumsSeeder.php
@@ -1,15 +1,16 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Flynsarmy\CsvSeeder\CsvSeeder;
 
 class AlbumsSeeder extends CsvSeeder {
-	
+    
+    public $insert_chunk_size = 1000;
+
 	public function __construct()
 	{
 		$this->table = 'albums';
 		$this->csv_delimiter = ',';
-		$this->filename = base_path().'/database/seeds/csvs/albums.csv';
+		$this->filename = base_path().'/database/seeds/csvs/albums2.csv';
 		$this->offset_rows = 1;
 		$this->mapping = [
 			0 => 'AlbumId',
@@ -25,9 +26,16 @@ class AlbumsSeeder extends CsvSeeder {
 		DB::disableQueryLog();
 
 		// Uncomment the below to wipe the table clean before populating
-		//DB::table($this->table)->truncate();
+		DB::table($this->table)->truncate();
 
 		parent::run();
 	}
+
+    public function readRow(array $row, array $mapping)
+    {
+        $row_values = parent::readRow($row, $mapping);
+        $row_values['AlbumReleaseDate'] = date('Y-m-d H:i:s', strtotime($row_values['AlbumReleaseDate']));
+        return $row_values;
+    }
 }
 
