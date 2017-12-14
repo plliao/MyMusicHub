f21769d1d5b3c04acf279ef8684ebcd6e3b1dd79
charson <chl566@nyu.edu>
2017-12-14 17:38:52 -0500

    reveals user info at home page

--- MyMusicHub/resources/views/home.blade.php
+++ MyMusicHub/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">MyMusicHub</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,20 @@
                         </div>
                     @endif
 
-                    You are logged in!
+		   <html>
+    		     <body>
+                    	You are logged in!  Hello, {{ Auth::user()->name }}
+    	             </body>
+		   </html>
+		   <h3>User Information:</h3>
+		   <?php foreach( $info as $row){?> 
+		   <ul>
+        		<li>User Account: <?php echo $row->username; ?></li>
+        		<li>Name: <?php echo $row->name; ?></li>
+        		<li>Email: <?php echo $row->email; ?></li>
+			<li>City: <?php echo $row->city; ?></li>
+    		   </ul>
+		   <?php }?>
                 </div>
             </div>
         </div>
