d852883dfb7958f4c0b2f93cf5c321dc4a6924fe
charson <chl566@nyu.edu>
2017-12-14 12:55:54 -0500

    modify the schema create order

---
+++ MyMusicHub/database/migrations/2017_12_14_160000_Tracks.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Tracks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+	Schema::create ( 'Tracks', function (Blueprint $table) { 
+	$table->string ( 'TrackId' )->primary();
+	$table->string ( 'TrackName' );
+	$table->integer( 'TrackDuration');
+	$table->string ( 'TrackArtist' );
+	$table->string ( 'AlbumId' );
+	$table->foreign('TrackArtist')->references('ArtistId')->on('Artists');	
+	$table->foreign('AlbumId')->references('AlbumId')->on('Albums');
+    	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+	Schema::dropIfExists('Tracks');
+    }
+}
