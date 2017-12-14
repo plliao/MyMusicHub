09eb0cc5c69ffabb3439e3eb143ce98e949b389e
Pei-Lun Liao <pll273@nyu.edu>
2017-12-14 17:43:14 -0500

    rename TrackTableSeeder to TracksSeeder

--- MyMusicHub/database/seeds/DatabaseSeeder.php
+++ MyMusicHub/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AlbumsSeeder::class);
-        $this->call(TrackTableSeeder::class);
+        $this->call(TracksSeeder::class);
     }
 }
 
