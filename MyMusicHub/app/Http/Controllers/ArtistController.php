f9c692e7094dae30bcd6c34acf9dbc7fdd6b6073
Pei-Lun Liao <pll273@nyu.edu>
2017-12-15 03:09:36 -0500

    add get route for search result

--- MyMusicHub/app/Http/Controllers/ArtistController.php
+++ MyMusicHub/app/Http/Controllers/ArtistController.php
@@ -7,6 +7,11 @@ use DB;
 
 class ArtistController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     //
 	public function show(Request $request)
     	{
