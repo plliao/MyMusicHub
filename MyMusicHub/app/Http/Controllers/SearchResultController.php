5f17c5e4f85385848b056e6717e21320f214614e
charson <chl566@nyu.edu>
2017-12-14 22:45:11 -0500

    add SearchResultController

---
+++ MyMusicHub/app/Http/Controllers/SearchResultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Input;
+
+class SearchResultController extends Controller
+{
+    //
+	public function show()
+    	{
+		$data = Input::all();
+		$keyword = $data['keyword'];
+		$artists = DB::select('select * from Artists where ArtistTitle like ?', [$keyword]);
+
+        	return view('SearchResultPage', ['result' => $artists]);
+  	}
+}
