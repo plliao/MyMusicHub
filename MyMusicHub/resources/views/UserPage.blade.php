ac6e0b571ded3d7ee032d78ac67f3ebc6f325ad4
Pei-Lun Liao <pll273@nyu.edu>
2017-12-15 20:24:53 -0500

    add follow view

--- MyMusicHub/resources/views/UserPage.blade.php
+++ MyMusicHub/resources/views/UserPage.blade.php
@@ -11,8 +11,34 @@
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
+                        {{ session()->forget('status') }}
                     @endif
-		   <h3><?php echo $userName ?>'s PlayLists:</h3>
+		   <h3><?php echo $userinfo->username; ?></h3>
+		   <h4>City: <?php echo $userinfo->city; ?></h4>
+           <form class="form-horizontal" method="POST" action="{{ action('UserPageController@follow') }}">
+                {{ csrf_field() }}
+                <input id="username" type="hidden" name="username" 
+                    value=<?php echo $userinfo->username; ?> />
+                <?php
+                    if (!$follower)
+                    {
+                ?>
+                    <input id="follow" type="hidden" name="follow" value=1 />
+                    <button type="submit" class="btn">
+                        Follow
+                    </button>
+                <?php
+                    } else {
+                ?>
+                    <input id="follow" type="hidden" name="follow" value=0 />
+                    <button type="submit" class="btn">
+                        UnFollow
+                    </button>
+                <?php
+                    }
+                ?>
+           </form>
+		   <h4>PlayLists:</h4>
 		   <?php foreach( $playList as $row){?> 
 		         <ul>
 		   	   <li>
